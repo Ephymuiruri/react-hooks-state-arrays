ff3d5de89db82ac1c70c0d9435a2c6dd17c4766b
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-04-26 07:42:18 +0300

    all deliverables met

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -7,10 +7,24 @@ function SpicyFoodList() {
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
     console.log(newFood);
+    const newFoodArray = [...foods,newFood]
+    setFoods(newFoodArray)
+  }
+  function HandleClick({id,name,cuisine,heatLevel}){
+    console.log(id)
+    const DlArray =foods.map((food)=> {
+      if (food.id ===id){
+        return{
+          id,name,cuisine,heatLevel:
+          heatLevel =heatLevel+1
+        }}
+       else {return food}
+      })
+    setFoods(DlArray)
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={()=>HandleClick(food)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
